681b08e871e902954ce55260d023edd8ce1a9ef6
D0683497 <ff129043923@gmail.com>
2021-06-18 23:56:00 +0800

    將個人資訊相關功能獨立出來

--- TsheThauLoo/Controllers/Account/AccountController.cs
+++ TsheThauLoo/Controllers/Account/AccountController.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
@@ -20,13 +18,9 @@ using Microsoft.IdentityModel.Tokens;
 using TsheThauLoo.Data;
 using TsheThauLoo.Dtos.Account;
 using TsheThauLoo.Dtos.Account.Login;
-using TsheThauLoo.Dtos.Account.National;
-using TsheThauLoo.Dtos.File;
 using TsheThauLoo.Entities.User;
 using TsheThauLoo.Utilities;
 using TsheThauLoo.Validator.Account;
-using TsheThauLoo.Validator.Account.File;
-using TsheThauLoo.Validator.Account.National;
 
 namespace TsheThauLoo.Controllers.Account
 {
@@ -42,7 +36,6 @@ namespace TsheThauLoo.Controllers.Account
         private readonly IConfiguration _configuration;
         private readonly IWebHostEnvironment _environment;
         private readonly TsheThauLooDbContext _dbContext;
-        private readonly IMapper _mapper;
 
         public AccountController(
             ILogger<AccountController> logger, 
@@ -51,8 +44,7 @@ namespace TsheThauLoo.Controllers.Account
             RoleManager<ApplicationRole> roleManager, 
             IConfiguration configuration, 
             IWebHostEnvironment environment, 
-            TsheThauLooDbContext dbContext, 
-            IMapper mapper)
+            TsheThauLooDbContext dbContext)
         {
             _logger = logger;
             _userManager = userManager;
@@ -61,7 +53,6 @@ namespace TsheThauLoo.Controllers.Account
             _configuration = configuration;
             _environment = environment;
             _dbContext = dbContext;
-            _mapper = mapper;
         }
         
         [AllowAnonymous]
@@ -258,310 +249,6 @@ namespace TsheThauLoo.Controllers.Account
                 return NoContent();
             }
             return BadRequest(result.Errors);
-        }
-
-        [AuthAuthorize]
-        [HttpGet("national", Name = nameof(National))]
-        public async Task<ActionResult<NationalDto>> National()
-        {
-            var userId = User.Claims
-                .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            var entity = await _dbContext.Users
-                .AsNoTracking()
-                .SingleOrDefaultAsync(x => x.Id == userId);
-            var dto = _mapper.Map<NationalDto>(entity);
-            return Ok(dto);
-        }
-        
-        [AuthAuthorize]
-        [HttpPost("national", Name = nameof(EditNational))]
-        public async Task<ActionResult<NationalDto>> EditNational([FromBody] NationalEditDto dto)
-        {
-            NationalEditDtoValidator validator = new NationalEditDtoValidator();
-            ValidationResult result = await validator.ValidateAsync(dto);
-            if (result.IsValid)
-            {
-                var userId = User.Claims
-                    .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-                var entity = await _dbContext.Users
-                    .SingleOrDefaultAsync(x => x.Id == userId);
-
-                #region 驗證
-
-                switch (entity.IdentityConfirmed)
-                {
-                    case true:
-                    {
-                        if (entity.NationalId != dto.NationalId)
-                        {
-                            result.Errors.Add(new ValidationFailure("nationalId", "身份證字號禁止修改"));
-                        }
-                        if (entity.Name != dto.Name)
-                        {
-                            result.Errors.Add(new ValidationFailure("name", "姓名禁止修改"));
-                        }
-                        if (entity.Gender != dto.Gender)
-                        {
-                            result.Errors.Add(new ValidationFailure("gender", "性別禁止修改"));
-                        }
-                        if (entity.DateOfBirth != dto.DateOfBirth)
-                        {
-                            result.Errors.Add(new ValidationFailure("dateOfBirth", "生日禁止修改"));
-                        }
-                        break;
-                    }
-                    case false:
-                    {
-                        if (entity.NationalId != dto.NationalId && !string.IsNullOrEmpty(dto.NationalId))
-                        {
-                            if (await _userManager.Users.AnyAsync(x => x.NationalId == dto.NationalId.ToUpper()))
-                            {
-                                result.Errors.Add(new ValidationFailure("nationalId", "身份證字號已經被使用"));
-                            }
-                        }
-                        break;
-                    }
-                }
-                if (!result.IsValid)
-                {
-                    return BadRequest(result.Errors);
-                }
-
-                #endregion
-
-                var updateEntity = _mapper.Map(dto, entity);
-                _dbContext.Users.Update(updateEntity);
-                await _dbContext.SaveChangesAsync();
-                var returnDto = _mapper.Map<NationalDto>(updateEntity);
-                return CreatedAtAction(nameof(National), null, returnDto);
-            }
-            return BadRequest(result.Errors);
-        }
-        
-        [AuthAuthorize]
-        [HttpGet("national/verify", Name = nameof(NationalVerify))]
-        public async Task<ActionResult<NationalVerifyDto>> NationalVerify()
-        {
-            var userId = User.Claims
-                .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            var entity = await _dbContext.Users
-                .AsNoTracking()
-                .Include(x => x.NationalVerify)
-                .Include(x => x.NationalVerify.NationalVerifyFiles)
-                .SingleOrDefaultAsync(x => x.Id == userId);
-            var dto = _mapper.Map<NationalVerifyDto>(entity);
-            return Ok(dto);
-        }
-        
-        [AuthAuthorize]
-        [HttpPost("national/verify", Name = nameof(EditNationalVerify))]
-        public async Task<ActionResult<NationalVerifyDto>> EditNationalVerify([FromBody] NationalEditVerifyDto dto)
-        {
-            NationalEditVerifyDtoValidator validator = new NationalEditVerifyDtoValidator();
-            ValidationResult result = await validator.ValidateAsync(dto);
-            if (result.IsValid)
-            {
-                var userId = User.Claims
-                    .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-                var user = await _dbContext.Users
-                    .Include(x => x.NationalVerify)
-                    .Include(x => x.NationalVerify.NationalVerifyFiles)
-                    .SingleOrDefaultAsync(x => x.Id == userId);
-                if (user.IdentityConfirmed)
-                {
-                    return Problem(title: "禁止修改", detail: "已通過實名驗證", statusCode: 403);
-                }
-                var updateEntity = _mapper.Map(dto, user.NationalVerify);
-                _dbContext.NationalVerifies.Update(updateEntity);
-                await _dbContext.SaveChangesAsync();
-                var returnDto = _mapper.Map<NationalVerifyDto>(user);
-                return CreatedAtAction(nameof(NationalVerify), null, returnDto);
-            }
-            return BadRequest(result.Errors);
-        }
-        
-        [AuthAuthorize]
-        [RequestFormLimits(ValueLengthLimit  = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
-        [HttpPost("national/verify/files", Name = nameof(CreateNationalVerifyFile))]
-        public async Task<ActionResult<FileDto>> CreateNationalVerifyFile([FromForm] FileCreateDto dto)
-        {
-            FileCreateDtoValidator validator = new FileCreateDtoValidator();
-            ValidationResult result = await validator.ValidateAsync(dto);
-            if (result.IsValid)
-            {
-                var userId = User.Claims
-                    .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-                var user = await _dbContext.Users
-                    .Include(x => x.NationalVerify)
-                    .Include(x => x.NationalVerify.NationalVerifyFiles)
-                    .SingleOrDefaultAsync(x => x.Id == userId);
-                if (user.IdentityConfirmed)
-                {
-                    return Problem(title: "禁止修改", detail: "已通過實名驗證", statusCode: 403);
-                }
-                var entity = _mapper.Map<NationalVerifyFile>(dto);
-
-                #region 處理檔案
-
-                await using (var transaction = await _dbContext.Database.BeginTransactionAsync())
-                {
-                    try
-                    {
-                        using (var stream = new FileStream(entity.Path, FileMode.Create))
-                        {
-                            await dto.FileData.CopyToAsync(stream);
-                        }
-                        user.NationalVerify.NationalVerifyFiles.Add(entity);
-                        _dbContext.Users.Update(user);
-                        await _dbContext.SaveChangesAsync();
-                        await transaction.CommitAsync();
-                    }
-                    catch (IOException)
-                    {
-                        await transaction.RollbackAsync();
-                        throw;
-                    }
-                    catch (DbUpdateException)
-                    {
-                        System.IO.File.Delete(entity.Path);
-                        await transaction.RollbackAsync();
-                        throw;
-                    }
-                }
-
-                #endregion
-
-                var returnDto = _mapper.Map<FileDto>(entity);
-                var routeValues = new {fileId = returnDto.Id};
-                return CreatedAtAction(nameof(NationalVerifyFile), routeValues, returnDto);
-            }
-            return BadRequest(result.Errors);
-        }
-        
-        [AuthAuthorize]
-        [HttpPost("national/verify/files/{fileId}", Name = nameof(EditNationalVerifyFile))]
-        public async Task<ActionResult<FileDto>> EditNationalVerifyFile([FromRoute] string fileId, [FromBody] FileEditDto dto)
-        {
-            FileEditDtoValidator validator = new FileEditDtoValidator();
-            ValidationResult result = await validator.ValidateAsync(dto);
-            if (result.IsValid)
-            {
-                var userId = User.Claims
-                    .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-                var user = await _dbContext.Users
-                    .Include(x => x.NationalVerify)
-                    .Include(x => x.NationalVerify.NationalVerifyFiles)
-                    .SingleOrDefaultAsync(x => x.Id == userId);
-                if (user.IdentityConfirmed)
-                {
-                    return Problem(title: "禁止修改", detail: "已通過實名驗證", statusCode: 403);
-                }
-                var entity = user.NationalVerify.NationalVerifyFiles
-                    .SingleOrDefault(x => x.NationalVerifyFileId == fileId);
-                if (entity == null)
-                {
-                    return NotFound();
-                }
-                var updateEntity = _mapper.Map(dto, entity);
-                _dbContext.NationalVerifyFiles.Update(updateEntity);
-                await _dbContext.SaveChangesAsync();
-                var returnDto = _mapper.Map<FileDto>(updateEntity);
-                var routeValues = new {fileId = returnDto.Id};
-                return CreatedAtAction(nameof(NationalVerifyFile), routeValues, returnDto);
-            }
-            return BadRequest(result.Errors);
-        }
-        
-        [AuthAuthorize]
-        [HttpGet("national/verify/files/{fileId}", Name = nameof(NationalVerifyFile))]
-        public async Task<ActionResult<FileDto>> NationalVerifyFile([FromRoute] string fileId)
-        {
-            var userId = User.Claims
-                .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            var user = await _dbContext.Users
-                .AsNoTracking()
-                .Include(x => x.NationalVerify)
-                .Include(x => x.NationalVerify.NationalVerifyFiles)
-                .SingleOrDefaultAsync(x => x.Id == userId);
-            var entity = user.NationalVerify.NationalVerifyFiles
-                .SingleOrDefault(x => x.NationalVerifyFileId == fileId);
-            if (entity == null)
-            {
-                return NotFound();
-            }
-            var dto = _mapper.Map<FileDto>(entity);
-            return Ok(dto);
-        }
-        
-        [AuthAuthorize]
-        [HttpGet("national/verify/files/{fileId}/download", Name = nameof(DownloadNationalVerifyFile))]
-        public async Task<IActionResult> DownloadNationalVerifyFile([FromRoute] string fileId)
-        {
-            var userId = User.Claims
-                .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            var user = await _dbContext.Users
-                .AsNoTracking()
-                .Include(x => x.NationalVerify)
-                .Include(x => x.NationalVerify.NationalVerifyFiles)
-                .SingleOrDefaultAsync(x => x.Id == userId);
-            var entity = user.NationalVerify.NationalVerifyFiles
-                .SingleOrDefault(x => x.NationalVerifyFileId == fileId);
-            if (entity == null)
-            {
-                return NotFound();
-            }
-            // 路徑、型態、下載的名稱
-            return File(System.IO.File.OpenRead(entity.Path), entity.Type, $"{entity.Name}{entity.Extension}");
-        }
-        
-        [AuthAuthorize]
-        [HttpDelete("national/verify/files/{fileId}", Name = nameof(DeleteNationalVerifyFile))]
-        public async Task<IActionResult> DeleteNationalVerifyFile([FromRoute] string fileId)
-        {
-            var userId = User.Claims
-                .Single(p => p.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
-            var user = await _dbContext.Users
-                .Include(x => x.NationalVerify)
-                .Include(x => x.NationalVerify.NationalVerifyFiles)
-                .SingleOrDefaultAsync(x => x.Id == userId);
-            if (user.IdentityConfirmed)
-            {
-                return Problem(title: "禁止修改", detail: "已通過實名驗證", statusCode: 403);
-            }
-            var entity = user.NationalVerify.NationalVerifyFiles
-                .SingleOrDefault(x => x.NationalVerifyFileId == fileId);
-            if (entity == null)
-            {
-                return NotFound();
-            }
-
-            #region 處理檔案
-
-            await using (var transaction = await _dbContext.Database.BeginTransactionAsync())
-            {
-                try
-                {
-                    _dbContext.NationalVerifyFiles.Remove(entity);
-                    await _dbContext.SaveChangesAsync();
-                    System.IO.File.Delete(entity.Path);
-                    await transaction.CommitAsync();
-                }
-                catch (IOException)
-                {
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-                catch (DbUpdateException)
-                {
-                    System.IO.File.Delete(entity.Path);
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            }
-
-            #endregion
-            
-            return NoContent();
         }
 
         private string GenerateJwtToken(IList<Claim> claims)
